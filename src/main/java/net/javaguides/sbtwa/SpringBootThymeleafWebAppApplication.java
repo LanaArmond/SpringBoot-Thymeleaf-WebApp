9937cb59fbddc33074b76d5277f18f1623d2c69f
LanaArmond <anabialana@gmail.com>
2026-03-26 08:33:46 -0300

    initial commit

---
+++ src/main/java/net/javaguides/sbtwa/SpringBootThymeleafWebAppApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.sbtwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootThymeleafWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/javaguides/sbtwa/SpringBootThymeleafWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.sbtwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
